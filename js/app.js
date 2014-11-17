8447872f1ebf6f54e7c545fc89809431344e358b
Rylan Bonnevie <rylanb13@uw.edu>
2014-11-17 12:16:18 -0800

    added google maps - markers

--- js/app.js
+++ js/app.js
@@ -6,3 +6,55 @@
 // lat: 47.655
 // lng: -122.3080
 
+$(document).ready(function () {
+   function createMap(center, zoom) {
+       var mapElem = document.getElementById('map');
+
+       var map = new google.maps.Map(mapElem, {
+           center: center,
+           zoom: zoom
+       });
+
+       var marker = new google.maps.Marker({
+          position: center,
+          map: map,
+           animation: google.maps.Animation.DROP
+       });
+
+       var infoWindow = new google.maps.InfoWindow();
+       infoWindow.setContent('<h2> Here I am!</h2>');
+
+       google.maps.event.addListener(marker, 'click', function() {
+          console.log('marker clicked!');
+           infoWindow.open(map, marker);
+           map.panTo(center);
+       });
+   }
+
+    function onGeoSuccess(position) {
+        var center = {
+            lat:position.coords.latitude,
+            lng: position.coords.longitude
+        };
+        createMap(center, 14);
+    }
+
+    function onGeoError(error) {
+        console.log(error);
+    }
+
+    var uwCoord = {
+        lat: 20.8,
+        lng: -156.333
+    };
+    createMap(uwCoord, 10);
+
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoError, {
+            enableHighAccuracy: true
+        });
+    } else {
+        createMap(uwCoord, 10);
+    }
+
+});
